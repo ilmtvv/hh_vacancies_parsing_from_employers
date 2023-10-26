5d67da257547c64be78f6419ad9b41e6ce39c77d
il <il@>
2023-10-26 21:11:09 +0300

    добавил создание таблиц

--- src/create_db.py
+++ src/create_db.py
@@ -8,7 +8,37 @@ con.autocommit = True
 
 cur = con.cursor()
 
+cur.execute(f"DROP DATABASE hh_vacancies")
+
 cur.execute(f"CREATE DATABASE hh_vacancies")
+
+cur.close()
+
+con.close()
+
+con = psycopg2.connect(dbname='hh_vacancies', **params)
+
+con.autocommit = True
+
+cur = con.cursor()
+
+cur.execute("""
+    CREATE TABLE employers (
+        id_employer INT PRIMARY KEY,
+        name_employer VARCHAR(100)
+    )
+""")
+
+cur.execute("""
+    CREATE TABLE vacancies (
+        id_employer INT,
+        id_vacancies SERIAL PRIMARY KEY,
+        name_vacancy VARCHAR(100),
+        url_vacancy VARCHAR(200),
+        salary INT
+    );
+    ALTER TABLE vacancies ADD CONSTRAINT fk_vacancies_employer FOREIGN KEY(id_employer) REFERENCES employers(id_employer)
+""")
 
 cur.close()
 
